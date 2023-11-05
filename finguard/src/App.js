42f38a737aae8bd4b0d5e8b61917d2b4a358bbb4
Harsh-Karia <64611524+Harsh-Karia@users.noreply.github.com>
2023-11-05 01:10:34 -0700

    shifted all of backend into one repo

--- finguard/src/App.js
+++ finguard/src/App.js
@@ -1,8 +1,6 @@
 
 import './App.css';
 import LandingPage from './components/LandingPage';
-import Register from './components/Register';
-import Login from './components/Login';
 import LoanRiskPredictor from './components/LoanRiskPredictor';
 
 function App() {
